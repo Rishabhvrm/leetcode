98da2ae6b8068b5536b43f068d9f7a0992241988
Rishabh Verma <rishabhvrm43@gmail.com>
2023-07-26 17:45:30 +0000

    climbing pictures fibonnaci

---
+++ 70.ClimbingStairs.py
@@ -0,0 +1,15 @@
+def climbStairs(n) -> int:
+        
+    # base case
+    if n == 0 or n == 1:
+        return 1
+
+    # table
+    dp = [0] * (n+1)
+    dp[0] = dp[1] = 1
+
+    # fibonnaci
+    for i in range(2, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+
+    return dp[n]
