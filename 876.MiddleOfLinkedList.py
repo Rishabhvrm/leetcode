27c7b86af8dd9419fb846359dc47321a2e396f9c
Rishabh Verma <rishabhvrm43@gmail.com>
2023-09-05 18:31:23 +0000

    add another solution

--- 876.MiddleOfLinkedList.py
+++ 876.MiddleOfLinkedList.py
@@ -1,7 +1,14 @@
 # trick: slow is always half of fast
 # kinda similar to 141.Linked List Cycle
 
-def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+    def __repr__(self):
+        return str(self.val)
+
+def middleNode(self, head: [ListNode]) -> [ListNode]:
     slow, fast = head, head
     
     while slow and fast and fast.next:     
@@ -9,3 +16,7 @@ def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         fast = fast.next.next
     
     return slow
+
+# ll = ListNode(1,2)
+# ll = ListNode(2,3)
+# print(ll)
