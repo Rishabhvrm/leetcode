f99787f1e50ab7ccf833e04cef7c801090e58fe2
Rishabh Verma <rishabhvrm43@gmail.com>
2023-09-06 21:47:11 +0000

    find all missing numbers

---
+++ 448.Find_all_numbers_disappered_in_an_array.py
@@ -0,0 +1,14 @@
+def findDisappearedNumbers(nums):
+    
+    set1, l = set(nums), len(nums)
+
+    # create second set, the ideal set with no missing values
+    set2 = set()
+    for ele in range(1, l+1):
+        set2.add(ele)
+
+    # return set difference
+    return list(set2-set1)
+
+nums = [4,3,2,7,8,2,3,1]
+print(findDisappearedNumbers(nums))
