0a77f79c58bcd6fc12b87be49f93ebf73ada0bee
Rishabh Verma <rishabhvrm43@gmail.com>
2023-09-06 23:29:26 +0000

    add comments

--- 70.ClimbingStairs.py
+++ 70.ClimbingStairs.py
@@ -4,12 +4,15 @@ def climbStairs(n) -> int:
     if n == 0 or n == 1:
         return 1
 
-    # table
+    # initialize table
     dp = [0] * (n+1)
     dp[0] = dp[1] = 1
 
     # fibonnaci
     for i in range(2, n+1):
         dp[i] = dp[i-1] + dp[i-2]
+        # print(dp)
 
     return dp[n]
+
+print(climbStairs(10))
