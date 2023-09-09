09ac78a5e901220073b424a14e9a50b1471f3b0c
Rishabh Verma <rishabhvrm43@gmail.com>
2023-09-09 16:42:54 +0000

    update DP solution

--- 303.Range_sum_query_immutable.py
+++ 303.Range_sum_query_immutable.py
@@ -1,20 +1,36 @@
-
 class NumArray:
 
-    def __init__(self, nums):
-        self.nums = nums
+    ##  BRUTE FORCE, T(N) = O (N * N) 
+
+    # def __init__(self, nums: List[int]):
+    #     self.nums = nums
         
+    # def sumRange(self, left: int, right: int) -> int:
+    #     # initialise sum
+    #     sm = 0
+        
+    #     # traverse from left to right (inclusive)
+    #     for i in range(left, right+1):
+    #         sm += self.nums[i]
+        
+    #     return sm
+
+    ## USING DP
+    ## STORE THE SUM WHILE INITIALISING
+    ## CONSTANT TIME WHILE CALLING sumRange()
+
+    def __init__(self, nums):
+        self.prefix = []
+        current_sum = 0
+        for ele in nums:
+            current_sum += ele
+            self.prefix.append(current_sum)
 
     def sumRange(self, left: int, right: int) -> int:
-        
-        # initialise sum
-        sm = 0
-        
-        # traverse from left to right (inclusive)
-        for i in range(left, right+1):
-            sm += self.nums[i]
-        
-        return sm
+        if left == 0:
+            return self.prefix[right] - 0
+        return self.prefix[right] -  self.prefix[left-1]
+    
 
 
 # Your NumArray object will be instantiated and called as such:
