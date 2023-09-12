474910ff29a773d7b46718e890a2973764d588ab
Rishabh Verma <rishabhvrm43@gmail.com>
2023-09-12 20:08:39 +0000

    palindrome LL better approach

--- 234.Palindrome_linked_list.py
+++ 234.Palindrome_linked_list.py
@@ -11,18 +11,63 @@ class Solution:
         ## SPACE COMPLEXITY: O(N), extra array to store reversed order
         ## CONVERT TO ARRAY
         ## REVERESE IT AND COMPARE
-        p = head
-        arr, rev_arr = [], []
+        
+        arr = []
         
         # convert to array
-        while p:
-            arr.append(p.val)
-            p = p.next
+        while head:
+            arr.append(head.val)
+            head = head.next
 
         # reverse array
-        rev_arr = arr[::-1]
+        # rev_arr = []
+        # rev_arr = arr[::-1]
 
-        if arr == rev_arr:
-            return True
+        l, r = 0, len(arr) -1
+        while l <= r:
+            if arr[l] != arr[r]:
+                return False
+            l += 1
+            r -= 1
+
+        return True
+
+        # if arr == rev_arr:
+        #     return True
+        
+    def isPalindrome2(self, head) -> bool:
+        
+        # T(N) = O(N)
+        # SPACE - O(1)
+
+        # FIND MIDDLE TO
+        # REVERSE SECOND HALF 
+        # POINT TO FIRST AND LAST AND CHECK FOR PALINDROME
+
+        # find middle using floyd's cycle detection algorithm
+        # also finds middle if no cycle (fast and slow pointers)
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # reverse second half
+        prev = None
+        while slow:                         # slow points to null
+            temp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = temp
+    
+        # second half of LL is reversed
+        # check palindrome
+        left, right = head, prev
+        while right:                        # until right reaches the middle i.e. None
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+        return True
 
         
