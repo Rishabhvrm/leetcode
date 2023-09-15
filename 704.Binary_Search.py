7570f4b19a0f7e6cc2085cf6b935d5799cbe398c
Rishabh Verma <rishabhvrm43@gmail.com>
2023-09-15 02:01:18 +0000

    add comments binary

--- 704.Binary_Search.py
+++ 704.Binary_Search.py
@@ -1,56 +1,54 @@
-
-
-    
-# recursive
-'''
-def search(self, nums: List[int], target: int) -> int:
-    if target not in nums:
-        return -1
-
-    l = len(nums)
-
-    # recursive solution
-    # if len is even
-    if l % 2 == 0:
-        # target is in first half
-        if nums[l//2 - 1] > target:                 
-            return self.search(nums[:l//2], target)
-
-        # target is in second half
-        else:                                       
-            return l//2 + self.search(nums[l//2:], target)
-
-    # if len is odd
-    else:
-        # middle element is target
-        if nums[l//2] == target:
-            return l//2
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
         
-        # target is in first half
-        if nums[l//2] > target:                     
-            return self.search(nums[:l//2], target)
-        
-        # target is in second half    
-        else:                                       
-            return l//2 + self.search(nums[l//2:], target)
-'''
+        # recursive
+        '''if target not in nums:
+            return -1
 
-def search(nums, target) -> int:
-    # two pointers
-    l = 0
-    r = len(nums) - 1
+        l = len(nums)
 
-    while l<=r:
-        mid = (l+r)//2
+        # recursive solution
+        # if len is even
+        if l % 2 == 0:
+            # target is in first half
+            if nums[l//2 - 1] > target:                 
+                return self.search(nums[:l//2], target)
+
+            # target is in second half
+            else:                                       
+                return l//2 + self.search(nums[l//2:], target)
+
+        # if len is odd
+        else:
+            # middle element is target
+            if nums[l//2] == target:
+                return l//2
             
-        if nums[mid] == target:
-            return mid
-        elif nums[mid] > target:
-            r = mid - 1 
-        elif nums[mid] < target:
-            l = mid + 1
+            # target is in first half
+            if nums[l//2] > target:                     
+                return self.search(nums[:l//2], target)
+            
+            # target is in second half    
+            else:                                       
+                return l//2 + self.search(nums[l//2:], target)
+'''
 
-    return -1
+        # two pointers
+        l = 0
+        r = len(nums) - 1
 
-l1 = [-1,0,3,5,9,12]
-print(search(l1, 5))
+        while l<=r:
+            # find middle index
+            mid = (l+r)//2
+                
+            # return if mid == target
+            if nums[mid] == target:
+                return mid
+            
+            # update l or r accordingly
+            elif nums[mid] > target:
+                r = mid - 1 
+            elif nums[mid] < target:
+                l = mid + 1
+
+        return -1
