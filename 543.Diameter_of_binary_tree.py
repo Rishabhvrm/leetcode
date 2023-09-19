1bffd83a9d0da37192e92e714f3ec780821e79d3
Rishabh Verma <rishabhvrm43@gmail.com>
2023-09-19 21:34:55 +0000

    add comments

--- 543.Diameter_of_binary_tree.py
+++ 543.Diameter_of_binary_tree.py
@@ -5,7 +5,16 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+        ## TIME COMPLEXITY: O(N)
+        ## SPACE COMPLEXITY: O(1)
+        ##                 : O(N) if consider recursion stack
+
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        
+        # uses the same concept as in
+        # https://leetcode.com/problems/maximum-depth-of-binary-tree/
+        # calculate diameter using left_depth + right_depth
+
         def dfs(node):
             nonlocal max_D
             if not node:
@@ -14,6 +23,7 @@ class Solution:
             left_depth = dfs(node.left)
             right_depth = dfs(node.right)
 
+            # calculate depth
             max_D = max(max_D, left_depth + right_depth)
 
             return 1 + max(left_depth, right_depth)
