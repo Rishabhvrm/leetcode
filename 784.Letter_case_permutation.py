7f0c6ec27bd0b7dd7806749182628723fbad53be
Rishabh Verma <rishabhvrm43@gmail.com>
2023-10-18 20:35:08 +0000

    add recursive solution

--- 784.Letter_case_permutation.py
+++ 784.Letter_case_permutation.py
@@ -1,19 +1,45 @@
 from typing import List
 def letterCasePermutation(s: str) -> List[str]:
 
-    # APPROACH1- Iterative
+    ## TIME COMPLEXITY: O(2 ^ N), for both approaches
+     
+    ## APPROACH 1: Iterative
 
-    out = [""] # needs to be a empty string in it
+    # out = [""] # needs to be a empty string in it
 
-    for c in s:
-        temp = []
-        if c.isdigit():
-            for o in out:
-                temp.append(o+c)
-        else:
-            for o in out:
-                temp.append(o+c.lower())
-                temp.append(o+c.upper())   
-        out = temp
+    # for c in s:
+    #     temp = []
+    #     if c.isdigit():
+    #         for o in out:
+    #             temp.append(o+c)
+    #     else:
+    #         for o in out:
+    #             temp.append(o+c.lower())
+    #             temp.append(o+c.upper())   
+    #     out = temp
     
-    return out
+    # return out
+
+    ## APPROACH 2: RECURSIVE
+
+    # output array
+    permutations = []
+
+    def backtrack(idx, ls):
+
+        # base case
+        if idx == len(s):
+            return permutations.append(ls)
+
+        # check if the ele is digit or alphabet
+        ele = s[idx]
+        if ele.isdigit():
+            backtrack(idx + 1, ls + ele)
+        # ele is alphabet
+        else:
+            backtrack(idx + 1, ls + ele.lower())
+            backtrack(idx + 1, ls + ele.upper())
+
+    backtrack(0, "")
+    return permutations
+
