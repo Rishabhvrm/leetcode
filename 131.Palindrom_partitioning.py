b171dcd7d5de73dabc88fdfb5a9f95e23eafed2c
Rishabh Verma <rishabhvrm43@gmail.com>
2023-10-23 21:44:20 +0000

    palindrome partitioning backtracking substring

--- 131.Palindrom_partitioning.py
+++ 131.Palindrom_partitioning.py
@@ -1,27 +1,33 @@
 from typing import List
 class Solution:
+
+    ## APPROACH: decision tree, check palindrome, substring
+    ## TIME: O(2 ^ N), each char can be part of palindrome or not => 2 choices
+    ## SPACE: O(N), recursion stack = depth of tree, curr_part => upto N
+
+    def is_palindrome(self, s):
+        return s == s[::-1]
+
     def partition(self, s: str) -> List[List[str]]:
-        
-        def isPali(s, left, right):
-            while left < right:
-                if s[left] != s[right]: return False
-                left += 1
-                right += 1
-            return True
 
         def backtrack(start, curr_part):
+
+            # base case
             if start == len(s):
                 res.append(curr_part[:])
                 return
-            
-            for idx in range(start, len(s)):
-                if isPali(s, start, idx):
-                    curr_part.append(s[start : idx + 1])
-                    backtrack(idx + 1, curr_part)
+
+            for end in range (start, len(s)):
+                substring = s[start : end + 1]
+
+                # only make tree further if substring is palindrome
+                if self.is_palindrome(substring):
+                    curr_part.append(substring)
+                    backtrack(end + 1, curr_part)
                     curr_part.pop()
 
         res = []
-        backtrack(0, [])
+        backtrack(0,[])
         return res
 
 s = Solution()
