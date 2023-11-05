4b4a07aa710840693cb2161d35c7cdb8dc405cf6
Rishabh <rishabhvrm43@gmail.com>
2023-11-05 00:01:28 -0400

    feat(add file): house robber 2 dp medium

---
+++ 213.House_robber_II.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        ## APPROACH: use house robber 1 on nums[1:] and nums[:-1]
+        ## TIME: O(N)
+        ## SPACE: O(1)
+
+        def helper(nums):
+            rob1, rob2 = 0, 0
+
+            # [..., rob1, rob2, n, n+1, ... ]
+            for n in nums:
+                rob_max = max(n + rob1, rob2)
+                rob1 = rob2
+                rob2 = rob_max
+
+            return rob2
+
+        #                   skip first value, skip last value
+        return max(nums[0], helper(nums[1:]), helper(nums[:-1]))
