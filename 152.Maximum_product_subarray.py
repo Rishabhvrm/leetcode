fb9f1967570a6408a3701d10d00fc370c7b4a703
Rishabh <rishabhvrm43@gmail.com>
2023-11-14 16:38:24 -0500

    refactor: revisit

--- 152.Maximum_product_subarray.py
+++ 152.Maximum_product_subarray.py
@@ -9,6 +9,26 @@ class Solution:
             curr_max = max( n * curr_max, n * curr_min, n )         # [-1, 8]
             curr_min = min( n * last_curr_max, n * curr_min, n )    # [-1, -8]
             res = max( curr_max, res )
-        
         return res
-       
+    
+    ## REVISIT
+    ## APPROACH: keep track of max and min at each step
+    # and calculate the next max based on that
+
+    ## TIME: O(n)
+    ## SPACE: O(1)
+    def maxProduct2(self, nums):
+        max_prod, curr_max, curr_min = nums[0], 1, 1
+        for n in nums:
+            prev_curr_max = curr_max
+            curr_max = max(curr_min * n, curr_max * n, n)
+            curr_min = min(curr_min * n, prev_curr_max * n, n)
+            max_prod = max(max_prod, curr_max)
+        return max_prod       
+    
+obj = Solution()
+nums=[2,3,-2,4]
+nums = [-2, 0, -1]
+nums = [-1, 8]
+nums = [1, -8]
+print(obj.maxProduct2(nums))
