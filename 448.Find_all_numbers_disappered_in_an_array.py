fb87dd4c3823361c6321eed4263db3641be240f6
Rishabh <rishabhvrm43@gmail.com>
2023-12-29 15:26:11 -0500

    refactor: mark -ve logic

--- 448.Find_all_numbers_disappered_in_an_array.py
+++ 448.Find_all_numbers_disappered_in_an_array.py
@@ -11,5 +11,14 @@ def findDisappearedNumbers(nums):
     # return set difference
     return list(set2-set1)
 
+def findDisappearedNumbers_mark(nums):
+    # mark the elements
+    for n in nums:
+        idx = abs(n) - 1                          # find idx
+        nums[idx] = -1 * abs(nums[idx])           # make that value -ve
+
+    return [i + 1 for i, n in enumerate(nums) if n > 0]     # +ve vals are missing vals
+
 nums = [4,3,2,7,8,2,3,1]
 print(findDisappearedNumbers(nums))
+print(findDisappearedNumbers_mark(nums))
