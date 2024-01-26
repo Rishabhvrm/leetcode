71101c26f372e1a0efe728f0be322823e17983e2
Rishabh <rishabhvrm43@gmail.com>
2024-01-25 19:18:02 -0500

    add min heap

--- fundamentals/heaps.py
+++ fundamentals/heaps.py
@@ -1,2 +1,23 @@
 # heap basics
+# using python's built-in min-heap implementation
+ 
+import heapq
+
+heap = []
+
+heapq.heappush(heap, 1)
+heapq.heappush(heap, 2)
+heapq.heappush(heap, 3)
+
+print("Heap:", heap)
+
+# get min ele without removing
+print("min ele:", heap[0])
+
+print("Heap:", heap)
+
+# removing min ele
+min_ele = heapq.heappop(heap)
+print("Extracted min:", min_ele)
+print("Heap after extraction:", heap)
 
