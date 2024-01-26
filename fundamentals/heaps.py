352e42aa2468de51b14aa5425c78741796f0355a
Rishabh <rishabhvrm43@gmail.com>
2024-01-25 19:25:55 -0500

    add max heap

--- fundamentals/heaps.py
+++ fundamentals/heaps.py
@@ -21,3 +21,13 @@ min_ele = heapq.heappop(heap)
 print("Extracted min:", min_ele)
 print("Heap after extraction:", heap)
 
+# max-heap
+mheap = []
+
+heapq.heappush(mheap, -1)
+heapq.heappush(mheap, -2)
+heapq.heappush(mheap, -3)
+
+print("mheap:", mheap)
+print("max element:", -1 * heapq.heappop(mheap))
+print("mheap:", mheap)
