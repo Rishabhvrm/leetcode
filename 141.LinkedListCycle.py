3adcfdcb9fab91c3d852e63b03e00efa0292ac42
Rishabh <rishabhvrm43@gmail.com>
2024-01-30 19:36:54 -0500

    refactor: add set solution

--- 141.LinkedListCycle.py
+++ 141.LinkedListCycle.py
@@ -1,18 +1,38 @@
 # FLOYD'S CYCLE-FINDING ALGORITHM
 # FLOYD'S hare and tortoise algo (fast and slow pointers)
 # faster will catch up slower one eventually
+from typing import Optional
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 def hasCycle(head: Optional [ListNode]) -> bool:
+    # using fast and slow pointers (floyd's)
+    # T(N) = O(n)
+    # Space = O(n)
     if head:
         slow, fast = head, head
 
-        while slow and fast and fast.next:
+        while fast and fast.next:
+            # update pointers
             slow = slow.next
             fast = fast.next.next
 
             # if tortoise and hare are at same place
             # there is a cycle
-            if slow == fast:
-                return True
+            if slow == fast: return True
 
         return False
+    
+    ## using set
+    # T(N) = O(n)
+    # Space = O(n)
+    visited = set()
+    curr = head
+    while curr:
+        if curr in visited: return True         # found a cycle
+        else: visited.add(curr)
+        curr = curr.next
+    return False
