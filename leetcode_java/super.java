60ed8a1a8692bf063089e8f8cda575b0f851586f
Rishabh <rishabhvrm43@gmail.com>
2024-02-01 01:09:12 -0500

    add super

---
+++ leetcode_java/super.java
@@ -0,0 +1,54 @@
+package leetcode_java;
+
+/*
+ * 'super' keyword allows for accessing 
+ * a) superclass members,
+ * b) invoking superclass constructors,
+ * c) invoking overridden superclass methods 
+ */
+
+class Test {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.display();
+        d.eat();
+    }
+}
+
+class Animal {
+    String color = "white";
+
+    Animal() {
+        System.out.println("Animal Constructor");
+    }
+    
+    void display() {
+        System.out.println("I am an Animal");
+    }
+
+    void eat() {
+        System.out.println("Animal is eating");
+    }
+}
+
+class Dog extends Animal {
+    String color = "black";
+
+    Dog() {
+        super();
+        System.out.println("Dog Constructor");
+    }
+
+    void display() {
+        System.out.println("I am a Dog");
+        System.out.println("Color: " + color);   // accessing subclass field
+        System.out.println("Superclass color: " + super.color);   // accessing superclass field
+        super.display();  // invoking superclass method
+    }
+
+    void eat(){
+        // Accessing Superclass Methods Overridden by the Subclass:
+        super.eat();  // Invoking superclass method
+        System.out.println("Dog is eating");
+    }
+}
