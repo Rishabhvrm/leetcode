08a60ed0573b2c5054c45cd856f3eaf69e5805b0
Rishabh <rishabhvrm43@gmail.com>
2024-02-10 16:41:03 -0500

    add zip() and refactor print

--- fundamentals/learning_new_functions.py
+++ fundamentals/learning_new_functions.py
@@ -1,9 +1,10 @@
 '''
 reverse function: traverse in reverse without using indexes
 '''
+print("\n---------------------------\nreversed():")
 l = [1,2,3,4,5]
 for n in reversed(l): print(n)
-print("\n---------------------------\n")
+
 
 
 '''
@@ -15,16 +16,25 @@ def generator_function():
     yield 'third-val'
     yield 4
 
+print("\n---------------------------\nyield:")
 my_generator = generator_function()
 for val in my_generator: print(val)
-print("\n---------------------------\n")
-
 
 
 
 '''
-itertools.izip_longest
+zip function
 '''
+print("\n---------------------------\nzip():")
+z1, z2 = [1,2,3,4], ["FIRST", "SECOND", "THIRD"]
+for ele in zip(z1,z2): print(ele)   # notice, missing '4' in the output
+
+
+
+'''
+itertools.zip_longest
+'''
+print("\n---------------------------\nzip_longest():")
 import itertools
 
 itr1 = [1,2,3]
@@ -32,4 +42,3 @@ itr2 = ['a','b']
 itr3 = ['FIRST']
 
 for item in itertools.zip_longest(itr1, itr2, itr3, fillvalue=None): print(item)
-print("\n---------------------------\n")
