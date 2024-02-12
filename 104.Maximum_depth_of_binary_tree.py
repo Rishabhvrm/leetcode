822c363db199fe78d61994564f2e70ffea82d61e
Rishabh <rishabhvrm43@gmail.com>
2024-02-12 16:08:17 -0500

    refactor LC 104, add iterative approach

--- 104.Maximum_depth_of_binary_tree.py
+++ 104.Maximum_depth_of_binary_tree.py
@@ -1,38 +1,44 @@
+from typing import Optional
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
 #         self.right = right
 class Solution:
+    '''
+    APPROACH-1: Recursion
+    calculate the depth at leaf nodes first
+    and keep adding/returning that root node 
+    TIME: O(N)
+    SPACE: O(1), O(N) if consider recursion stack
+    '''
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-
-        ## TIME COMPLEXITY: O(N)
-        ## SPACE COMPLEXITY: O(1)
-        ##                 : O(N) if consider recursion stack
-
-        if not root:
-            return 0
-
+        if not root: return 0
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
 
 
-        # def count_nodes(node, node_count):
-            # if no node, return 0
-            # if not node:
-            #     return 0
+    '''
+    APPROACH-2: Iterative + Recursion
+    count the depth at current node first
+    keep increasing the count until reach the leaf node
+    while returning, return whatever the max depth is from the left and right subtree
+    TIME: O(N)
+    SPACE: O(1), O(N) if consider recursion stack
+    '''
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        def count_nodes(node, node_count):
+            if not node: return 0 
 
-            # # increase node_count as soon as encounter a node
-            # node_count += 1
+            # increase node_count as soon as encounter a node
+            node_count += 1
 
-            # # return node_count when encounter leaf node
-            # if not node.left and not node.right:
-            #     return node_count
-            
-            # # calculate node_count for left and right subtrees
-            # return max(count_nodes(node.left, node_count), 
-            #             count_nodes(node.right, node_count))
+            # return node_count when encounter leaf node
+            if not node.left and not node.right: return node_count
 
-
-        # return count_nodes(root, 0)
+            # calculate node_count for left and right subtrees
+            return max(count_nodes(node.left, node_count),
+                        count_nodes(node.right, node_count))
         
+        return count_nodes(root, 0)
+
