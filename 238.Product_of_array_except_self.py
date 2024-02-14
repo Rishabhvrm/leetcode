949dc3a80ca7ae1ff9ff494e91fdc5554bb5ba7c
Rishabh <rishabhvrm43@gmail.com>
2024-02-14 18:28:53 -0500

    refactor, add another approach

--- 238.Product_of_array_except_self.py
+++ 238.Product_of_array_except_self.py
@@ -1,25 +1,43 @@
+from typing import List
 
-def productExceptSelf(nums):
+class Solution:
+    '''
+    APPROACH-1: Brute Force
+    TIME: O(N ^ 2)
+    SPACE: O(1)
+    '''
+    def productExceptSelf(self, nums: List[int]) -> List[int]: 
+        res, n = [], len(nums)
+
+        for i in range(n):
+            product = 1
+            for j in range(n):
+                if i != j: product *= nums[j]
+            res.append(product)
+
+        return res
     
-    ## APPROACH: calculate right and left products for each element in array
-    ## T(N): O(N)
-    ## SPACE: O(N)
+    '''
+    APPROACH-2: calculate right and left products for each element in array
+    T(N): O(N)
+    SPACE: O(N)
+    '''
+    def productExceptSelf(nums):
+        n = len(nums)
+        left_product, right_product = 1, 1
 
-    n = len(nums)
-    left_product, right_product = 1, 1
+        # initialize arrays
+        prefix, suffix = [1] * n, [1] * n
 
-    # initialize arrays
-    prefix, suffix = [1] * n, [1] * n
+        # calculate and store left products in prefix
+        for i in range(n):
+            prefix[i] = left_product
+            left_product *= nums[i]
 
-    # calculate and store left products in prefix
-    for i in range(n):
-        prefix[i] = left_product
-        left_product *= nums[i]
-
-    # calculate and store right products in suffix
-    for i in range(n-1, -1, -1):
-        suffix[i] = right_product
-        right_product *= nums[i]
-    
-    # use prefix and suffix to calculate result
-    return [prefix[i] * suffix[i] for i in range(n)]
+        # calculate and store right products in suffix
+        for i in range(n-1, -1, -1):
+            suffix[i] = right_product
+            right_product *= nums[i]
+        
+        # use prefix and suffix to calculate result
+        return [prefix[i] * suffix[i] for i in range(n)]
