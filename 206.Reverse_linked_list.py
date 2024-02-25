a24f2e9f103fd479954530308e2dafbd0d57af42
Rishabh <rishabhvrm43@gmail.com>
2024-02-25 16:06:21 -0500

    refactor LC 206

--- 206.Reverse_linked_list.py
+++ 206.Reverse_linked_list.py
@@ -8,6 +8,7 @@ class ListNode:
     def __repr__(self):
         return str(self.val)
 
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         # REVISIT
@@ -23,3 +24,42 @@ class Solution:
             curr = nxt              # update curr
 
         return prev
+    
+
+
+class Solution:
+    '''
+    APPROACH-1: ITERATIVE
+    TIME: O(N)
+    SPACE: O(1)
+    '''
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, curr, nxt = None, head, None
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        return prev
+
+    '''
+    APPROACH-2: RECURSIVE
+    TIME: O(N)
+    SPACE: O(1)
+    '''
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # logic for start node
+        # logic for generic nodes (all the nodes in between)
+        # logic for end node
+        def reverse(prev, curr):
+            if curr.next == None:                   # BASE CASE / END NODE
+                curr.next = prev
+                return curr
+            
+            head = reverse(curr, curr.next)         # Logic for generic and first node
+            curr.next = prev
+            return head
+
+        if not head: return None
+        return reverse(None, head)
+        
