7e5b8c53bac82e4633a01cb185ca15b9601b453a
Rishabh <rishabhvrm43@gmail.com>
2024-02-27 23:43:33 -0500

    refactor LC 141, add approaches

--- 141.LinkedListCycle.py
+++ 141.LinkedListCycle.py
@@ -8,31 +8,51 @@ class ListNode:
         self.val = x
         self.next = None
 
-def hasCycle(head: Optional [ListNode]) -> bool:
-    # using fast and slow pointers (floyd's)
-    # T(N) = O(n)
-    # Space = O(n)
-    if head:
-        slow, fast = head, head
-
-        while fast and fast.next:
-            # update pointers
-            slow = slow.next
-            fast = fast.next.next
-
-            # if tortoise and hare are at same place
-            # there is a cycle
-            if slow == fast: return True
-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+        
+class Solution:
+    '''
+    APPROACH-1: use a set to keep track of visited nodes
+    TIME: O(N)
+    SPACE: O(N), set()
+    '''
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        visited = set()
+        curr = head             # pointer to loop/traverse the list
+        while curr:
+            if curr in visited: return True
+            visited.add(curr)
+            curr = curr.next
         return False
-    
-    ## using set
-    # T(N) = O(n)
-    # Space = O(n)
-    visited = set()
-    curr = head
-    while curr:
-        if curr in visited: return True         # found a cycle
-        else: visited.add(curr)
-        curr = curr.next
-    return False
+        
+    '''
+    APPROACH-2: change the value of visited nodes
+    if a node with changed val is encountered, there's cycle present
+    note that it will change the original LL values
+    TIME: O(N)
+    SPACE: O(1)
+    '''
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        curr = head
+        while curr:
+            if curr.val == 10 ** 5 + 1: return True
+            curr.val = 10 ** 5 + 1
+            curr = curr.next
+        return False
+
+    '''
+    APPROACH-3: floyd's warshall LL cycle, if hare catches tortoise, it's a cycle
+    TIME: O(N)
+    SPACE: O(1)
+    '''
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+            if slow == fast: return True
+        return False
+        
