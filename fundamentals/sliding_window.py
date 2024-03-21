cd911717dedffac86f0688b346b58f97b64a98ab
Rishabh <rishabhvrm43@gmail.com>
2024-03-21 15:31:43 -0400

    revisit

--- fundamentals/sliding_window.py
+++ fundamentals/sliding_window.py
@@ -16,3 +16,20 @@ def max_subarray_sum(nums, k) -> int:
 
 nums = [1,2,3,4,5,6]
 print(max_subarray_sum(nums, 3))
+
+def revisit_sliding_window(nums, k) -> int:
+    if k > len(nums):
+        return nums
+
+    max_sum = 0
+    curr_sum = sum(nums[:k])
+    #  i = [0,1,2]
+    for i in range(len(nums) - k):
+        curr_sum -= nums[i]
+        curr_sum += nums[i + k]
+        max_sum = max(max_sum, curr_sum)
+
+    return max_sum
+    #   0 1 2 3 4 5
+nums = [1,2,3,4,5,6]
+print(revisit_sliding_window(nums, 3))
