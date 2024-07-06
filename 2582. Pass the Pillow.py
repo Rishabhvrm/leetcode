33362387d65ad44ff74b79d74883d5372f944832
Rishabh <rishabhvrm43@gmail.com>
2024-07-06 14:22:26 -0700

    refactor: LC 2582, add math approach

--- 2582. Pass the Pillow.py
+++ 2582. Pass the Pillow.py
@@ -1,7 +1,11 @@
 class Solution:
     '''
+          #  
+    1 2 3 4
+    0
+
     Approach: Simulate
-    Time: O(n)
+    Time: O(time)
     Space: O(1)
     '''
     def passThePillow(self, n: int, time: int) -> int:
@@ -9,19 +13,37 @@ class Solution:
 
         while time:
             if idx == n:
-                dir = -1
+                dir = -1      # L <-- R
             elif idx == 1:
-                dir = 1
+                dir = 1       # L --> R
             
             idx += dir
             time -= 1
 
         return idx
-    
+
     '''
+        #
+    1 2 3 4
+
+    n = 3, t = 2
+    remaining_steps = t % (n-1)
+    decide end
+
+    
+    if time // (n-1) is even, left end
+    if time // (n -1) is odd, right end
+
+    (n - 1) steps to reach right end, 
+    2 * (n - 1) steps to reach left end
+
     Approach: Math
     Time: O(1)
     Space: O(1)
     '''
     def passThePillow(self, n: int, time: int) -> int:
-        pass
+        end = "left" if (time // (n - 1)) % 2 == 0 else "right"
+
+        remaining_steps = time % (n - 1)
+
+        return 1 + remaining_steps if end == "left" else n - remaining_steps
