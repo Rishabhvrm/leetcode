94f6c0d56dd141edc1f4ed0be3875fcde8ddd521
Rishabh <rishabhvrm43@gmail.com>
2024-07-31 00:45:59 -0700

    add LC 2330

---
+++ 2330. Valid Palindrome IV.py
@@ -0,0 +1,63 @@
+'''
+2330. Valid Palindrome IV
+
+You are given a 0-indexed string s consisting of only lowercase English letters. In one operation, you can change any character of s to any other character.
+
+Return true if you can make s a palindrome after performing exactly one or two operations, or return false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "abcdba"
+Output: true
+Explanation: One way to make s a palindrome using 1 operation is:
+- Change s[2] to 'd'. Now, s = "abddba".
+One operation could be performed to make s a palindrome so return true.
+Example 2:
+
+Input: s = "aa"
+Output: true
+Explanation: One way to make s a palindrome using 2 operations is:
+- Change s[0] to 'b'. Now, s = "ba".
+- Change s[1] to 'b'. Now, s = "bb".
+Two operations could be performed to make s a palindrome so return true.
+Example 3:
+
+Input: s = "abcdef"
+Output: false
+Explanation: It is not possible to make s a palindrome using one or two operations so return false.
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s consists only of lowercase English letters.
+'''
+
+'''
+    L R 
+a b d d b a
+
+
+    L       
+h b z x g n
+      R 
+
+
+'''
+class Solution:
+    def makePalindrome(self, s: str) -> bool:
+        operations = 2
+        l, r = 0, len(s) - 1
+        
+        while l < r:
+            if s[l] != s[r]:
+                operations -= 1
+                if operations < 0:
+                    return False
+            
+            l, r = l + 1, r - 1
+        
+        return True
+        
