7b4fdc26b61aadac0480bfde57054e9472ca94cf
Rishabh <rishabhvrm43@gmail.com>
2024-08-13 17:01:55 -0700

    refactor: add comments

--- 40.Combinaton_sum_II.py
+++ 40.Combinaton_sum_II.py
@@ -1,4 +1,30 @@
 from typing import List
+
+'''
+I:
+    +ve, and >= 1
+    repeat? => yes
+    target >= 1
+
+O:
+    [1], t = 4 => Output = [  ] if no solution exist
+    [1,1], t = 2 => output = [ [1,1] ]
+
+APPROACHES:
+    Give TLE, repeated work
+    1: 2 choices at every level,
+            stop when sum goes beyond or find ans or idx goes beyond len
+            remove duplicates by using a set while adding to final res
+    
+    Good backtracking approach, skips duplicates
+    2: left skewed tree where from any point there are only branches forward
+            skip duplicate branches, need to be sorted so that adjacent elements can be compared
+            use a for loop and compare current and prev branch
+'''
+'''
+approach-1: complete binary tree with 2 choices at every node
+TLE at 172/176
+'''
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
 
