1c24a02e3c806cb643325925b79fdd12d94e711b
Rishabh <rishabhvrm43@gmail.com>
2024-08-23 17:25:27 -0400

    refactor: add approaches

--- 476. Number Complement.py
+++ 476. Number Complement.py
@@ -1,4 +1,20 @@
+from math import log
+
 class Solution:
+
+    '''
+    Approach: XOR each digit with 1 to flip it. One-by-one
+    Time: O(log n)
+    Space: O(1)
+    '''
+    def findComplement(self, num: int) -> int:
+        bit_len = int(log(num, 2)) + 1
+        
+        for i in range(bit_len):
+            num ^= 1 << i
+        return num
+
+
     '''
     Idea: XOR a digit with 1 to flip it
     Time: O(log n)
@@ -10,3 +26,21 @@ class Solution:
         mask = (1 << bit_len) - 1
 
         return num ^ mask
+
+    '''
+    Approach: XOR each digit with 1 to flip it. One-by-one
+    Time: O(log n)
+    Space: O(1)
+    '''
+    def findComplement(self, num: int) -> int:
+        i = 0
+        comp = 0
+
+        while num:
+            if(num & 1 == 0):     # flip
+                comp |= 1 << i
+
+            num >>= 1
+            i += 1
+
+        return comp
