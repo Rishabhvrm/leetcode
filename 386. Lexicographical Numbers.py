e774db52ed5597677eb4333789b4d6ea066d8639
Rishabh <rishabhvrm43@gmail.com>
2024-09-21 17:36:53 -0400

    add LC 386

---
+++ 386. Lexicographical Numbers.py
@@ -0,0 +1,17 @@
+from typing import List
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        res = []
+
+        def dfs(curr):
+            if curr > n:
+                return
+            
+            res.append(curr)
+            
+            for i in range(10):
+                dfs(curr * 10 + i)
+
+        for i in range(1, 10):  
+            dfs(i)
+        return res
