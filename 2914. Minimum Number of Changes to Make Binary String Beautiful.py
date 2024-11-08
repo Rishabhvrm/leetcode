75091b46b60318fb8b791636bd4c4a7982dbe77b
Rishabh <rishabhvrm43@gmail.com>
2024-11-07 19:04:13 -0500

    refactor LC 2914

--- 2914. Minimum Number of Changes to Make Binary String Beautiful.py
+++ 2914. Minimum Number of Changes to Make Binary String Beautiful.py
@@ -14,11 +14,22 @@ class Solution:
         n = len(s)
         count = 0
 
+        # if adjacent elements are both 1 or both 0
+        # skip them
+        # else increment the count (they must be different)
+        # for i in range(0, n - 1, 2):
+        #     if ((s[i] == "1" and s[i + 1] == "1") or
+        #         (s[i] == "0" and s[i + 1] == "0")):
+        #         continue
+        #     else:
+        #         count += 1
+                
+        # using this instead of above logic (that is correct too)
+        # just check if adjacent elements are equal or not
+        # if they're not then we have to change one of them in order to make 
+        # the string beautiful
         for i in range(0, n - 1, 2):
-            if ((s[i] == "1" and s[i + 1] == "1") or
-                (s[i] == "0" and s[i + 1] == "0")):
-                continue
-            else:
+            if s[i] != s[i + 1]:
                 count += 1
 
         return count
