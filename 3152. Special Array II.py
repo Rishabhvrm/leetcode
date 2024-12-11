7956f32b6a9e27be83d416b8c32fa89a5ec477db
Rishabh <rishabhvrm43@gmail.com>
2024-12-10 19:40:24 -0500

    update LC 3152

--- 3152. Special Array II.py
+++ 3152. Special Array II.py
@@ -28,3 +28,32 @@ class Solution:
             res.append(bool_res)
 
         return res
+
+    '''
+    Approach-2: Prefix, calculate the res before hand 
+    res: till this position how many mismatches have you seen
+    given start and end we can find if there are no violating indexes btw them or not
+    if prefix[end] - prefix[start] == 0
+    Time: O(n)
+    Space: O(n)
+    '''
+    # [a,b,c,d,e]
+    # [_,T,F,T,F]
+    # 4,3,1,6,2
+    # _,T,F,T,F
+    # _,0,1,1,2
+    def isArraySpecial(self, nums: List[int], queries: List[List[int]]) -> List[bool]:
+        res = []
+        prefix = [0] * len(nums)
+
+        for i in range(1, len(nums)): 
+            if self.diff_parity(nums[i], nums[i - 1]):
+                prefix[i] = prefix[i - 1]
+            else:
+                prefix[i] = prefix[i - 1] + 1
+
+        for start, end in queries:
+            res.append(prefix[end] - prefix[start] == 0)
+
+        return res
+
