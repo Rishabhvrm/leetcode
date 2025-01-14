517a3bebdddc4f8982c9097b165b560b0cdf4298
Rishabh <rishabhvrm43@gmail.com>
2025-01-14 18:07:53 -0500

    refactor: add approach LC 1422

--- 1422. Maximum Score After Splitting a String.py
+++ 1422. Maximum Score After Splitting a String.py
@@ -37,3 +37,26 @@ class Solution:
             res = max(res, curr_res)
             
         return res
+    
+    '''
+    one pass
+    '''
+    def maxScore(self, s: str) -> int:
+        zeros, ones = 0, 0
+        for c in s:
+            if c == "1":
+                ones += 1
+
+        res = 0
+        for i in range(len(s) - 1):
+            c = s[i]
+            if c == "0":
+                zeros += 1
+            else:
+                ones -= 1
+            
+            res = max(res, zeros + ones)
+
+        return res
+
+
